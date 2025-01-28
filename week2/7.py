2e99ce99104a12401b341ebdc6735fd19a33b22b
Your Name <a_karimova@kbtu.kz>
2025-01-28 14:44:16 +0500

    Python if..else

---
+++ week2/7.py
@@ -0,0 +1,14 @@
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+'''
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+'''
