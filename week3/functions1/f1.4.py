66ae5260686ac49b8d61adee24c5b8d40d60f071
Your Name <a_karimova@kbtu.kz>
2025-02-01 16:40:46 +0500

    Task1

--- week3/functions1/f1.4.py
+++ week3/functions1/f1.4.py
@@ -1,6 +1,28 @@
 def filter_prime(numbers):
+    new_numbers = []
+
     for i in numbers:
         if i < 2 :
-            return False
-        else:
-            for j in range(2,  )
+            continue 
+
+        f = True
+        for j in range(2, (int(i**0.5) + 1)):
+            if i % j == 0 :
+                f = False
+                break
+        if f :
+            new_numbers.append(i)
+
+    return new_numbers 
+
+
+nums = []
+n = int(input("List length:"))
+for i in range(n):
+    el = int(input())
+    nums.append(el)
+
+
+print(filter_prime(nums))
+
+
