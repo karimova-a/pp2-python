c9756d974b78b482a507e7f03241f6368182d79b
Your Name <a_karimova@kbtu.kz>
2025-02-22 03:14:53 +0500

    Week4

--- week4/json/j.1.py
+++ week4/json/j.1.py
@@ -1,1 +1,24 @@
-print('a')
+import json
+
+def openn(pyfile):
+    with open(pyfile, 'r') as file:
+        return json.load(file)
+
+def print_(data):
+    print("Interface Status")
+    print("=" * 80)
+    print(f"{'DN':<50} {'Description':<20} {'Speed':<8} {'MTU'}")
+    print("-" * 80)
+    
+    
+    for i in data["imdata"]:
+        attributes = i['l1PhysIf']['attributes']
+        DN = attributes['dn']
+        description = attributes.get('descr', '')
+        speed = attributes['speed']
+        MTU = attributes['mtu']
+        print(f"{DN:<50} {description:<20} {speed:<10} {MTU:<10}")
+
+
+data = openn('sample-data.json')   
+print_(data)
